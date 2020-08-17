dbdceb3fe2af8108f558caaf92d756a2d6bbcadd
luiz-ortega <luizngust@hotmail.com>
2020-08-17 20:59:05 -0300

    feat: reducing subTotal prices to totalPrice

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -47,9 +47,13 @@ const Cart: React.FC = () => {
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((acc, product) => {
+      const productSubtotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return acc + productSubtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
